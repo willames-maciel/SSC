ffa090454dfb39fe8e5f6dbb723317f477ba5d9a
Paulo Victor <paulo.victor.s.sousa01@gmail>
2024-10-16 12:00:33 -0300

    feat:Modificação na tela de Relatório,Assuntos e Subassuntos. Criação do componete MyInput afim de otimizar.

--- src/app/pages/sub-matters/sub-matters.component.ts
+++ src/app/pages/sub-matters/sub-matters.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, ViewEncapsulation, input } from '@angular/core';
+import { MyInputComponent } from '../../my-input/my-input.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-sub-matters',
   standalone: true,
-  imports: [],
+  imports: [MyInputComponent],
   templateUrl: './sub-matters.component.html',
-  styleUrl: './sub-matters.component.scss'
+  styleUrl: './sub-matters.component.scss',
+  template: '<input type="text"/>',
+  styles: [
+    `
+      input{
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  width: 100%;
+  background-color: red;
+}
+
+    `,
+  ],
+  encapsulation: ViewEncapsulation.Emulated,
+
 })
 export class SubMattersComponent {
+  email : string = '';
+  nome : string = '';
+
+
 
 }
