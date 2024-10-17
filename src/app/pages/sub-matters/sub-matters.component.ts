e86058ee4bee2510503e6c260f89d877f119db44
Paulo Victor <paulo.victor.s.sousa01@gmail>
2024-10-17 11:57:21 -0300

    feat:Modificaçao na tela de Assuntos e Subassuntos.

--- src/app/pages/sub-matters/sub-matters.component.ts
+++ src/app/pages/sub-matters/sub-matters.component.ts
@@ -1,5 +1,4 @@
 import { Component, NgModule, ViewEncapsulation, input } from '@angular/core';
-import { MyInputComponent } from '../../my-input/my-input.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 
@@ -8,28 +7,15 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-sub-matters',
   standalone: true,
-  imports: [MyInputComponent],
+  imports: [],
   templateUrl: './sub-matters.component.html',
   styleUrl: './sub-matters.component.scss',
-  template: '<input type="text"/>',
-  styles: [
-    `
-      input{
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  width: 100%;
-  background-color: red;
-}
+  template: '<input type="text"/>'
 
-    `,
-  ],
-  encapsulation: ViewEncapsulation.Emulated,
 
 })
 export class SubMattersComponent {
-  email : string = '';
-  nome : string = '';
+
 
 
 
